a0411e3efce004fc9250bfb511ddf37a3e0c7061
Sung-ju Kim <goddoe@goddoe2@gmail.com>
2018-08-25 01:08:43 +0900

    update cnn

--- cnn/cnn.py
+++ cnn/cnn.py
@@ -1,10 +1,16 @@
+import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
 
 class CNN(nn.Module):
 
     def __init__(self):
-        pass
+        self.conv1 = nn.Conv1d(1, 6, kernel_size=3)
+        self.conv2 = nn.Conv1d(6, 16, kernel_size=3)
+        self.conv2_drop = nn.Dropout1d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
 
     def forward(self, X):
-        pass
+        X = self.conv1(X)
+
